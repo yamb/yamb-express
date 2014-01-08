2c39cc9efc415b0170f24656d9b4a0abd7955f6a
Alexey Simonenko <dwarfman@gmail.com>
2014-01-08 21:57:38 +0400

    Configure port and dbname for app

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 "use strict";
 
+var config = require('./config/app');
+
 var express = require('express');
 var engine  = require('express-hbs');
+
+var mongo = require('co-easymongo')(config.dbname);
 
 var routes = require('./routes');
 var server = express();
 
 routes(server);
 
-require('http').createServer(server).listen(3000, function() {
-  console.log("Run server on port 3000");
+require('http').createServer(server).listen(config.port, function() {
+  console.log('Run server on ' + config.port + ' port');
 });
