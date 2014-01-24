e62dc34058512fe327fbfe351c562614b6ea065b
Alexey Simonenko <dwarfman@gmail.com>
2014-01-24 19:37:05 +0400

    Add basic CRUD interface

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,17 +1,62 @@
 "use strict";
 
-exports.index = function(req, res) {
-  res.send('index action');
+exports.index = function *(req, res) {
+  var posts = yield res.yamb.fetchAll();
+
+  res.render('admin/index', {
+    posts: posts
+  });
 };
 
-exports.show = function(req, res) {
-  res.send('show action');
+exports.create = function *(req, res) {
+  var related = yield res.yamb.fetchAll(null, {sort: 'name'});
+
+  res.render('admin/show', {
+    post: {},
+    related: related
+  });
 };
 
-exports.update = function(req, res) {
-  res.send('update action');
+exports.save = function *(req, res) {
+  var post = res.yamb.create(req.body);
+
+  try {
+    yield post.save();
+  } catch (err) {
+    return res.redirect('/yamb/create?error=' + err.message);
+  }
+
+  res.redirect('/yamb/' + post.id);
 };
 
-exports.remove = function(req, res) {
-  res.send('remove action');
+exports.show = function *(req, res) {
+  var related = yield res.yamb.fetchAll({id: {$ne: res.post.id}}, {sort: 'name'});
+
+  console.log(res.post.json());
+
+  res.render('admin/show', {
+    related: related
+  });
+};
+
+exports.update = function *(req, res) {
+  if (!req.body.related) {
+    req.body.related = [];
+  }
+
+  var post = res.post.update(req.body);
+
+  try {
+    yield post.save();
+  } catch (err) {
+    return res.redirect('/yamb/' + post.id + '?error=' + err.message);
+  }
+
+  res.redirect('/yamb/' + post.id);
+};
+
+exports.remove = function *(req, res) {
+  var result = yield res.post.remove();
+
+  res.redirect('/yamb?result=' + result);
 };
--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,12 @@ module.exports = function(app, middleware) {
   var action = require('./../controllers/admin');
 
   app.get('/yamb', action.index);
+
+  app.get('/yamb/create', action.create);
+  app.post('/yamb/create', action.save);
+
   app.get('/yamb/:yamb', action.show);
   app.post('/yamb/:yamb', action.update);
-  app.post('/yamb/:yamb/remove', action.remove);
+
+  app.get('/yamb/:yamb/remove', action.remove);
 };
